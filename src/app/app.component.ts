4dff55ed91c173faba4b2d7a0c8117ae005aae49
Andrea Gonzato <gonzatoandrea98@gmail.com>
2024-10-14 23:13:24 +0200

    feat: add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {ContactPageComponent} from "./contact-page/contact-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, ContactPageComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {ContactPageComponent} from "./contact-page/contact-page.component";
+import {HomePageComponent} from "./home-page/home-page.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'contact', component: ContactPageComponent},
+  {path: 'home', component: HomePageComponent},
+  {path: '', component: HomePageComponent},
+];
