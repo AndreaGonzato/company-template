84f95c0dd39b926a756aefcf10300ddbfabba744
Andrea Gonzato <gonzatoandrea98@gmail.com>
2025-02-17 22:12:21 +0100

    refactor: remove unused AsyncPipe import and add Netlify deploy script

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,13 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {AsyncPipe} from "@angular/common";
 import {BackEndManagerService} from "../back-end-manager.service";
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
-    imports: [
-        AsyncPipe
-    ],
     templateUrl: './home-page.component.html'
 })
 export class HomePageComponent implements OnInit{
